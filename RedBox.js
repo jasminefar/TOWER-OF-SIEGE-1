90926e01832bc40cacb1a2276aad19286a624ea9
jasminefar <73764457+jasminefar@users.noreply.github.com>
2020-12-20 13:39:41 -0800

    commit

---
+++ RedBox.js
@@ -0,0 +1,11 @@
+class RedBox extends Box{
+    constructor(x,y){
+        super(x,y,20,30);
+    }
+
+    display(){
+        fill(233, 119, 121);
+        super.display();
+    }
+}
+//by Jasmine Far
---
+++ YellowBox.js
@@ -0,0 +1,11 @@
+class YellowBox extends Box{
+    constructor(x,y){
+        super(x,y,20,30);
+    }
+
+    display(){
+        fill(242, 217, 3);
+        super.display();
+    }
+}
+//by Jasmine Far
